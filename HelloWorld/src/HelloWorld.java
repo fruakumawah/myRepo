b776af60c4e2dcdecb1f6ab3a2d219a84ac57d22
Fru Akumawah <fru.akumawah@inxire.com>
2014-02-27 14:37:34 +0100

    Please save this

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Fru Akumawah
+ * 
+ */
+public class HelloWorld
+{
+
+	public void doSomething()
+	{
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world!");
+	}
+
+}
